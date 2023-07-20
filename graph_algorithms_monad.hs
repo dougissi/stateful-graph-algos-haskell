62b624836e6e04c51eb821c4e5a113c5344e2839
Doug Issichopoulos <41125998+dougissi@users.noreply.github.com>
2023-07-20 08:00:35 -0700

    remove Node type since Graph is sufficient

--- graph_algorithms_monad.hs
+++ graph_algorithms_monad.hs
@@ -1,18 +1,15 @@
 import Prelude as P
 import Data.Set as S
 import Data.IntMap.Strict as M
+import Data.Maybe
 import Control.Monad.State
 
 
-data Node = Node { val :: Int, neighbors :: [Int] }
-type Graph = M.IntMap Node
+type Graph = M.IntMap [Int]
 type VisitedNodes = S.Set Int
-type GraphState = Graph
 type Traversal = [Int]
 type TraversalState = (VisitedNodes, Traversal)
 
-instance Show Node where
-    show (Node a neighbors) = "Node " ++ show a ++ " " ++ show neighbors
 
 strToInt :: String -> Int
 strToInt = read
@@ -23,24 +20,20 @@ makeIntPair [s1,s2] = (strToInt s1, strToInt s2)
 
 
 formatGraphFile :: String -> [(Int, Int)]
-formatGraphFile s = 
+formatGraphFile s =
     let ls = lines s
         strPairs = P.map words ls
     in P.map makeIntPair strPairs
 
 
 connect :: Graph -> Int -> Int -> Graph
-connect g i1 i2 = 
-    let neighbors1 = aux1 i1 g
-        neighbors2 = aux1 i2 g
-        neighbors1' = aux2 i2 neighbors1
-        neighbors2' = aux2 i1 neighbors2
-        n1 = Node i1 neighbors1'
-        n2 = Node i2 neighbors2'
-    in M.insert i1 n1 (M.insert i2 n2 g)
-    where aux1 i g = case M.lookup i g of
-                        Nothing -> []
-                        Just (Node _ neighbors) -> neighbors
+connect g i1 i2 =
+    let n1 = aux1 i1 g
+        n2 = aux1 i2 g
+        n1' = aux2 i2 n1
+        n2' = aux2 i1 n2
+    in M.insert i1 n1' (M.insert i2 n2' g)
+    where aux1 i g = fromMaybe [] (M.lookup i g)
           aux2 i neighbors = if i `elem` neighbors
                                 then neighbors
                                 else i:neighbors
@@ -48,11 +41,11 @@ connect g i1 i2 =
 
 buildGraph :: [(Int, Int)] -> Graph
 buildGraph pairs = reverseNeighbors (aux pairs M.empty)
-    where 
+    where
         aux [] g = g
         aux ((i1, i2):is) g = aux is (connect g i1 i2)
-        reverseNeighbors = M.map (\(Node x neighbors) -> Node x (reverse neighbors))
-        
+        reverseNeighbors = M.map reverse
+
 
 traverseGraph :: Graph -> Int -> State TraversalState Traversal
 traverseGraph g root = do
@@ -65,18 +58,22 @@ traverseGraph g root = do
             (visited, t) <- get
             case M.lookup root g of
                 Nothing -> return t
-                Just (Node _ neighbors) -> do
+                Just neighbors -> do
                     put (S.insert root visited, t ++ [root])  -- consider root to be visited
-                    loop g neighbors            
-            
+                    loop g neighbors
+
         loop _ [] = do
             (_, t) <- get
             return t
 
         loop g (i:is) = do
             (visited, t) <- get
-            if i `elem` visited 
-                then return t
-                else dfs g i
+            if i `S.notMember` visited
+                then dfs g i
+                else return t  -- do nothing
             loop g is
-    
+
+trav g root =
+    let res = evalState (traverseGraph g root) (S.empty, [])
+    in res
+
