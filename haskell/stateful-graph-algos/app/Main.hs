60fa1a9b54d366b55cb0c1b535aa95a0659abb5e
Doug Issichopoulos <41125998+dougissi@users.noreply.github.com>
2023-07-30 19:35:46 -0700

    clean up REPL

--- haskell/stateful-graph-algos/app/Main.hs
+++ haskell/stateful-graph-algos/app/Main.hs
@@ -24,44 +24,41 @@ repl state@(g, impl, t, spl) = do
     putStr "> "
     hFlush stdout
     input <- getLine
-    if input == "exit" then
-        do putStrLn "Bye!"
-           return ()
-    else if input == "switch" then
-        do let (impl', t', spl') = toggleImplementation impl
-           putStrLn $ "switched to " ++ impl' ++ "ic implementation"
-           repl (g, impl', t', spl')
-    else if input == "viewGraph" then
-        do print g
-           repl state
-    else if input == "buildGraph" then
-        do putStr "enter a series of edges, such as `(1,2) (2,3)`:\n"
-           hFlush stdout
-           input <- getLine
-           let edgesStrings = words input
-               edges = map (\x -> read x :: Edge) edgesStrings
-           repl (buildGraph edges, impl, t, spl)
-    else if input == "buildGraphFromFile" then
-        do putStr "enter relative filepath: "
-           hFlush stdout
-           edgesFilename <- getLine
-           file <- openFile edgesFilename ReadMode
-           contents <- hGetContents file
-           let edges = formatEdgesFile contents
-           repl (buildGraph edges, impl, t, spl)
-    else if input == "traversal" then
-        do putStr "specify source node: "
-           hFlush stdout
-           input <- getLine
-           let src = read input :: Int
-           print (t g src)
-           repl state
-    else if input == "shortestPathLens" then
-        do print (spl g)
-           repl state
-    else do
-        putStrLn "unrecognized input"
-        repl state
+    case input of
+        "exit"               -> do putStrLn "Bye!"
+                                   return ()
+        "switch"             -> do let (impl', t', spl') = toggleImplementation impl
+                                   putStrLn $ "switched to " ++ impl' ++ "ic implementation"
+                                   repl (g, impl', t', spl')
+        "viewGraph"          -> do print g
+                                   repl state
+        "buildGraph"         -> do putStr "enter a series of edges, such as `(1,2) (2,3)`:\n"
+                                   hFlush stdout
+                                   edgesSeries <- getLine
+                                   let edgesStrings = words edgesSeries
+                                       edges = map (\x -> read x :: Edge) edgesStrings
+                                       g' = buildGraph edges
+                                   putStrLn $ "graph successfully built: " ++ show g'
+                                   repl (g', impl, t, spl)
+        "buildGraphFromFile" -> do putStr "enter relative filepath: "
+                                   hFlush stdout
+                                   edgesFilename <- getLine
+                                   file <- openFile edgesFilename ReadMode
+                                   contents <- hGetContents file
+                                   let edges = formatEdgesFile contents
+                                       g' = buildGraph edges
+                                   putStrLn $ "graph successfully built: " ++ show g'
+                                   repl (g', impl, t, spl)
+        "traversal"          -> do putStr "specify source node: "
+                                   hFlush stdout
+                                   srcStr <- getLine
+                                   let src = read srcStr :: Int
+                                   print (t g src)
+                                   repl state
+        "shortestPathLens"   -> do print (spl g)
+                                   repl state
+        _                    -> do putStrLn "unrecognized input"
+                                   repl state
 
 
 main :: IO ()
@@ -71,6 +68,7 @@ main = do putStrLn "Welcome to the Stateful Graph Algos REPL!\n"
           putStrLn "* switch -> switch between monadic and non-monadic implementations (default is monadic)"
           putStrLn "* buildGraph -> will be prompted to manually enter edges of the desired graph"
           putStrLn "* buildGraphFromFile -> will be prompted to enter the relative filepath to the txt file of graph edges"
+          putStrLn "* viewGraph -> print out the current graph" 
           putStrLn "* traversal -> after then being prompted to the starting node, will print out the in-order traversal from that node"
           putStrLn "* shortestPathLens -> will print out the shortest paths between all nodes (except those where no path exists)"
           repl (emptyGraph, "monad", GAM.traversal, GAM.shortestPathLens)
